e096c5147b3a83e737dcd96dc8911a6a5e1a698c
‘Mohamed’ <momobounouar1@gmail.com>
2022-06-23 02:01:25 +0200

    add Repository secrets

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ const cheerio = require('cheerio');
 
 
 
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
