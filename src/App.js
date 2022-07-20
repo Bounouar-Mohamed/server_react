9d32b794d404c208434d5294636b9a2baf25545c
‘Mohamed’ <momobounouar1@gmail.com>
2022-07-20 16:18:32 +0200

    comment catch error

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ app.post("/login", function (req, res,) {
   pool.query(
     `SELECT * FROM users WHERE email = $1 AND password= $2  `,
     [email, password], (error, result) => {
-      if (error) throw error;  //  <--- peut être ici
+      // if (error) throw error;  //  <--- peut être ici
 
 
       if (result.rows.length === 0) {
