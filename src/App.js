bb8a7a2f94fe28655a849c79f7e52052ba370570
‘Mohamed’ <momobounouar1@gmail.com>
2022-08-28 17:32:55 +0200

    Add API for delete Account

--- src/App.js
+++ src/App.js
@@ -164,17 +164,16 @@ app.post("/sneakers", function (req, res,) {
 
 app.post('/delete', function (req, res) {
 
-  // const id = parseInt(req.body.id)
+  const id = (req.body.id)
 
-  // console.log('req', req.body.id)
-  console.log('req', req.body.id)
+  console.log('req id', req.body.id)
 
 
-  // pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-  //   if (error) throw error;
-  //   res.json({ message: `User deleted with ID: ${id}` })
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) throw error;
 
-  // });
+    res.json({ message: `User deleted with ID: ${id}` })
+  });
 })
 
 
